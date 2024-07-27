4d35617050a3bcc40dbdeffb5bc8c0f326cc11e1
Quake <142627174+SJ-209@users.noreply.github.com>
2024-07-27 20:22:31 +0100

    updates on second portfolio website using js

--- zero-to-mastery/background-generator/script.js
+++ zero-to-mastery/background-generator/script.js
@@ -2,8 +2,20 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var btn = document.getElementById("random");
+
+
 
 // setting linear gradient function //
+
+body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ "," 
+	+ color2.value 
+	+ ")";  
+
+	css.textContent = body.style.background + ";";
 
 function setGradient() {
 	body.style.background = 
@@ -17,6 +29,21 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+
+function randmColor() {
+	let color = "#";
+  for(let i = 0; i < 6; i++){
+		color += Math.floor((Math.random() * 16)).toString(16);
+  }
+	return color;
+
+}
+
+
+random.addEventListener('click', () => {
+	color1.value=randmColor();
+	color2.value=randmColor();
+});
 
 // calling linear gradient function because DRY //
 
