9adadbc20b96ab8e7d408b95cfac62c817f5f74f
Quake <142627174+SJ-209@users.noreply.github.com>
2024-11-01 17:43:28 +0000

    using the fetch API

--- zero-to-mastery/robofriends/src/App.js
+++ zero-to-mastery/robofriends/src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import  { robots } from './robots';
+import './App.css';
+import { robots } from "./robots";
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => this.setState({ robots: users}));
+        
     }
 
     onSearchChange = (event) => {
@@ -21,14 +29,17 @@ class App extends Component {
         const filterRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className="tc">
-            <h1>RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <CardList robots={filterRobots} />
-            </div>
-    
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f2">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filterRobots} />
+                </div>
+            );
+        }
     }
 }
 
