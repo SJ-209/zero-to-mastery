8d593a4f81b562ac0ce65ef7d70a7363bc7b1292
Quake <142627174+SJ-209@users.noreply.github.com>
2024-11-21 07:23:37 +0000

    ex async await

--- zero-to-mastery/promises/exerciseAsyncAwait.js
+++ zero-to-mastery/promises/exerciseAsyncAwait.js
@@ -1,9 +1,13 @@
 // Solve the below problems:
 
 // #1) Convert the below promise into async await
-fetch("https://jsonplaceholder.typicode.com/users/")
-  .then((response) => response.json())
-  .then(console.log);
+
+async function fetchUsers(){
+  const resp = await fetch("https://jsonplaceholder.typicode.com/users/")
+  const data = await resp.json();
+  console.log(data);
+}
+
 
 // #2) ADVANCED: Update the function below from the video to also have
 // async await for this line: fetch(url).then(resp => resp.json())
@@ -16,12 +20,12 @@ const urls = [
 ];
 
 const getData = async function () {
-  const [users, posts, albums] = await Promise.all(
-    urls.map((url) => fetch(url).then((resp) => resp.json())),
-  );
-  console.log("users", users);
-  console.log("posta", posts);
-  console.log("albums", albums);
+    const [users, posts, albums] = await Promise.all(
+      urls.map((url) => fetch(url).then((resp) => resp.json())),
+    );
+    console.log("users", users);
+    console.log("posta", posts);
+    console.log("albums", albums);
 };
 
 // #3)Add a try catch block to the #2 solution in order to catch any errors. // Now, use the given array containing an invalid url, so you console.log  //your error with 'oooooops'.
@@ -30,3 +34,16 @@ const urls = [
   "https://jsonplaceholdeTYPO.typicode.com/posts",
   "https://jsonplaceholder.typicode.com/albums",
 ];
+
+const getData = async function () {
+  try {
+    const [users, posts, albums] = await Promise.all(
+      urls.map((url) => fetch(url).then((resp) => resp.json())),
+    );
+    console.log("users", users);
+    console.log("posta", posts);
+    console.log("albums", albums);
+  } catch {
+    console.log('oops', err)
+  }
+};
