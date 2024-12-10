dd411977f47eb1df603bbdedefbd3b79b17b0d70
Quake <142627174+SJ-209@users.noreply.github.com>
2024-12-10 22:19:28 +0000

    creating useState const for function app imageLInkForm

--- zero-to-mastery/facerecognitionbrain/src/App.js
+++ zero-to-mastery/facerecognitionbrain/src/App.js
@@ -5,19 +5,26 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [,setInput] = useState('');
+      const onInputChange = (event) => {
+        console.log(event.target.value);
+        setInput(event.target.value);
+      }
   return (
     <div className="App">
       <div>...</div>
       <ParticlesBg className='particles' color='#3ca9d1' blur='5' num={150} density type="cobweb" bg={true} />
-     <Navigation />
-     <Logo />
-     <Rank />
-     <ImageLinkForm />
+    <Navigation />
+    <Logo />
+    <Rank />
+    <ImageLinkForm onInputChange={onInputChange}/>
             {/*<FaceRecognition />*/ }
     </div>
   );
 }
+
 
 export default App;
--- zero-to-mastery/facerecognitionbrain/src/components/ImageLinkForm/ImageLinkForm.js
+++ zero-to-mastery/facerecognitionbrain/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './ImageLInkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange }) => {
     return (
         <div>
             <p className='f3'>
@@ -9,7 +9,7 @@ const ImageLinkForm = () => {
             </p>
             <div className='center'>
                 <div className=' form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='tex'></input>
+                    <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}></input>
                     <button className='w-30 grow f4 link ph3 pv2dib white bg-light-blue'>Detect</button>
                 </div>
             </div>
