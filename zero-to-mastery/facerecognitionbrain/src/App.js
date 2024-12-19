f0c55d4fa2e6ae98b22dd5b541c5ea415e7a7242
Quake <142627174+SJ-209@users.noreply.github.com>
2024-12-19 18:36:56 +0000

    Created Facerecognition, Navigation, Signin but still having problems with CORS block of API

--- zero-to-mastery/facerecognitionbrain/src/App.js
+++ zero-to-mastery/facerecognitionbrain/src/App.js
@@ -1,30 +1,125 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import Particles from 'react-particles-js'; 
 import ParticlesBg from 'particles-bg'
+// import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-import { useState } from 'react';
 
-function App() {
-  const [,setInput] = useState('');
-      const onInputChange = (event) => {
-        console.log(event.target.value);
-        setInput(event.target.value);
-      }
-  return (
-    <div className="App">
-      <div>...</div>
-      <ParticlesBg className='particles' color='#3ca9d1' blur='5' num={150} density type="cobweb" bg={true} />
-    <Navigation />
-    <Logo />
-    <Rank />
-    <ImageLinkForm onInputChange={onInputChange}/>
-            {/*<FaceRecognition />*/ }
-    </div>
-  );
+
+// const app = new Clarifai.App({
+//  apiKey: 'e3aee845766948ac9260f613e27189cf'
+// });
+
+    const returnClarifiRequestOptions = (imageUrl) => {
+        // Your PAT (Personal Access Token) can be found in the Account's Security section
+        const PAT = '79a63d3983084df98b1c50920769890c';
+        // Specify the correct user_id/app_id pairings
+        // Since you're making inferences outside your app's scope
+        const USER_ID = 'clarifai';       
+        const APP_ID = 'main';
+        // Change these to whatever model and image URL you want to use  
+        const IMAGE_URL = imageUrl;
+    
+        const raw = JSON.stringify({
+            "user_app_id": {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            "inputs": [
+                {
+                    "data": {
+                        "image": {
+                            "url": IMAGE_URL
+                        }
+                    }
+                }
+            ]
+        });
+    
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Key ' + PAT,
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': ' ',
+            },
+            body: raw
+        };
+    
+        return requestOptions
+    };
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].regions_info.bounding_box;
+    const image =document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height) 
+    };
+  }
+
+  displayFacebox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value }); // Update input state
+  }
+
+  onButtonSubmit = () => {
+      this.setState({imageUrl:this.state.input});
+      //app.models.predict('face-detection',this.state.input)
+      
+      fetch("https://api.clarifai.com/v2/models/face-detection/outputs" + returnClarifiRequestOptions(this.state.input))
+          .then(response => response.json())
+          .then(response => {this.displayFacebox( 
+            this.state.calculateFaceLocation(response));
+            console.log('hi', response)
+              
+          })
+          .catch(err => console.log('Error:', err));
+        }
+
+
+  render()  {
+    return (
+        <div className="App">
+        <div>...</div>
+            <ParticlesBg className='particles' color='#3ca9d1' blur='5' num={150} density type="cobweb" bg={true} />
+            <Navigation />
+            <Signin />
+            <Logo />
+            <Rank />
+            <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}
+            />
+              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        </div>
+    );
+  }
 }
-
 
 export default App;
--- zero-to-mastery/facerecognitionbrain/src/components/ImageLinkForm/ImageLinkForm.js
+++ zero-to-mastery/facerecognitionbrain/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './ImageLInkForm.css';
 
-const ImageLinkForm = ({ onInputChange }) => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className='f3'>
@@ -10,7 +10,10 @@ const ImageLinkForm = ({ onInputChange }) => {
             <div className='center'>
                 <div className=' form center pa4 br3 shadow-5'>
                     <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}></input>
-                    <button className='w-30 grow f4 link ph3 pv2dib white bg-light-blue'>Detect</button>
+                    <button 
+                        className='w-30 grow f4 link ph3 pv2dib white bg-light-blue'
+                        onClick={onButtonSubmit}
+                    >Detect</button>
                 </div>
             </div>
         </div> 
