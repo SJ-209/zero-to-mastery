8da4dc1ea00cfbff77e937929277e14a1cae8fcb
Quake <142627174+SJ-209@users.noreply.github.com>
2025-01-25 22:46:48 +0000

    fixing front end

--- zero-to-mastery/facerecognitionbrain/src/App.js
+++ zero-to-mastery/facerecognitionbrain/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-// import Particles from 'react-particles-js'; 
 import ParticlesBg from 'particles-bg'
-import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
@@ -11,23 +9,10 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-//You must add your own API key here from Clarifai.
-const app = new Clarifai.App({
- apiKey: 'e3aee845766948ac9260f613e27189cf'
-});
-
-// No Longer need this. Updated to particles-bg
-// const particlesOptions = {
-//   particles: {
-//     number: {
-//       value: 30,
-//       density: {
-//         enable: true,
-//         value_area: 800
-//       }
-//     }
-//   }
-// }
+const PAT = process.env.REACT_APP_CLARIFAI_API_KEY;
+const USER_ID = 'clarifai';
+const APP_ID = 'main';
+const MODEL_ID = 'face-detection';
 
 class App extends Component {
   constructor() {
@@ -81,32 +66,51 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-   
-    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-    // If that isn't working, then that means you will have to wait until their servers are back up. 
+    
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": this.state.input
+                    }
+                }
+            }
+        ]
+    });
 
-    app.models.predict('face-detection', this.state.input)
-      .then(response => {
-        console.log('hi', response)
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
-            })
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
 
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      })
-      .catch(err => console.log(err));
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            if (result) {
+                fetch('http://localhost:3001/image', {
+                    method: 'put',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                        id: this.state.user.id
+                    })
+                })
+                .then(response => response.json())
+                .then(count => {
+                    this.setState(Object.assign(this.state.user, { entries: count}))
+                })
+            }
+            this.displayFaceBox(this.calculateFaceLocation(result))
+        })
+        .catch(error => console.log('error', error));
   }
 
   onRouteChange = (route) => {
